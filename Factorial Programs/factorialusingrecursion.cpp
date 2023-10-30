bd61a28c66d372a0495ddc858d5c59d3b97dca13
Vivashwat Thakur <83788445+vivdto@users.noreply.github.com>
2023-10-30 19:37:46 +0000

    Program to Find the Factorial of a Number using Recursion

---
+++ Factorial Programs/factorialusingrecursion.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int fact(int n)
+{
+if(n == 0 || n ==1){
+return 1;
+}
+else
+return n*fact(n-1);
+}
+
+int main ()
+{
+    int num;
+    cout<<"Enter the number: ";
+    cin>>num;
+
+    cout<<"the factorial of "<<num<<"is "<< fact(num)<<endl;
+}
