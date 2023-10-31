6469c5e312f34094ce922f8938f398e1688a69da
Vivashwat Thakur <83788445+vivdto@users.noreply.github.com>
2023-10-31 10:47:07 +0000

    new program added.

---
+++ Data Types & Operators/sizeofpointer.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int * ip;
+    long * lp;
+    char * cp;
+    float * fp;
+    double * dp;
+ 
+
+    cout<<sizeof(ip)<<endl;
+    cout<<sizeof(cp)<<endl;
+    cout<<sizeof(fp)<<endl;
+    cout<<sizeof(dp)<<endl;
+    cout<<sizeof(lp)<<endl;
+
+
+return 0;
+}
