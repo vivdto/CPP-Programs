bde36c53adda8f63b45e583201247bab62c5b1e3
Vivashwat Thakur <83788445+vivdto@users.noreply.github.com>
2023-10-31 10:37:45 +0000

    sizeof program.

---
+++ Data Types & Operators/sizeof.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+
+    int i;
+    char ch;
+    float f;
+    double dub;
+    long lg;
+
+    cout<<sizeof(i)<<endl;
+    cout<<sizeof(ch)<<endl;
+    cout<<sizeof(f)<<endl;
+    cout<<sizeof(dub)<<endl;
+    cout<<sizeof(lg)<<endl;
+
+
+
+    return 0;
+}
