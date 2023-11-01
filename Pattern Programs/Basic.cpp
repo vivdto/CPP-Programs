44f8c41b2caf80a199285769003a643757d59dae
Vivashwat Thakur <83788445+vivdto@users.noreply.github.com>
2023-11-01 04:54:29 +0000

    Added new Programs for Pattern Questions.

---
+++ Pattern Programs/Basic.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+int main ()
+{
+    
+    int i;
+    cout<<"Enter any random number to print pattern: ";
+    cin>>i;
+    
+    
+    while (i--)
+    cout<<"*";
+    
+    
+    return 0;
+}
---
+++ Pattern Programs/basicpattern.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+int main ()
+{
+    
+    
+    int i;
+    cout<<"Enter any random number to print star pattern";
+    cin>>i;
+        
+        while(i--)
+        cout<<"*"<<endl;
+        
+        
+    return 0;
+}
