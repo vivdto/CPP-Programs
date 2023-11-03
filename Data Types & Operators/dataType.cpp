7da8285bb2a90e08fec2dadbb5181fcc678375af
Vivashwat Thakur <83788445+vivdto@users.noreply.github.com>
2023-11-03 06:18:38 +0000

    code enhanced.

--- Data Types & Operators/dataType.cpp
+++ Data Types & Operators/dataType.cpp
@@ -1,19 +1,58 @@
 #include<iostream>
 using namespace std;
 
-int dataType(string type){
+int main ()
 
-    if (type == "Integer")
-    return sizeof(int);
+{
+    
+    
+    string type;
+    
+    
+    cout<<"Enter a datatyep to check it's size: ";
+    cin>>type;
+    
+    if( type == "Integer" )
+    cout<<"The size of data is :"<<sizeof(int)<<endl;
+    
+    else if( type == "Float" )
+    cout<<"The size of data is :"<<sizeof(float)<<endl;
 
-    if (type == "Integer")
-    return sizeof(int);
+    
+    else if( type == "Dobule" )
+    cout<<"The size of data is :"<<sizeof(double)<<endl;
+    
+    
+    else if( type == "Long" )
+    cout<<"The size of data is :"<<sizeof(long)<<endl;
+    
+    
+    else if( type == "Character" )
+    cout<<"The size of "<<type<<"data is :"<<sizeof(char)<<endl;
+    
+    else
+    cout<<"Kindly chose one of the following"<<endl;
+    cout<<"Integer, Long, Character, Float, Dobule"<<endl;
+    
+    cin>>type;
+    if( type == "Integer" )
+    cout<<"The size of data is :"<<sizeof(int)<<endl;
+    
+    else if( type == "Float" )
+    cout<<"The size of data is :"<<sizeof(float)<<endl;
 
-if (type == "Integer")
-    return sizeof(int);
-
-if (type == "Integer")
-    return sizeof(int);
-
-
+    
+    else if( type == "Dobule" )
+    cout<<"The size of data is :"<<sizeof(double)<<endl;
+    
+    
+    else if( type == "Long" )
+    cout<<"The size of data is :"<<sizeof(long)<<endl;
+    
+    
+    else if( type == "Character" )
+    cout<<"The size of "<<type<<" datatype is :"<<sizeof(char)<<endl;
+    
+    
+    return 0;
 }
