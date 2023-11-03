f31228d8b7a1d571824bae183a2b76b834bbf6af
Vivashwat Thakur <83788445+vivdto@users.noreply.github.com>
2023-11-03 05:52:20 +0000

    added another program for datatype.

---
+++ Data Types & Operators/dataType.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int dataType(string type){
+
+    if (type == "Integer")
+    return sizeof(int);
+
+    if (type == "Integer")
+    return sizeof(int);
+
+if (type == "Integer")
+    return sizeof(int);
+
+if (type == "Integer")
+    return sizeof(int);
+
+
+}
