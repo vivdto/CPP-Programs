37f7f9a2f1cfee7a4e35b4ab8682ccb662c8af05
Vivashwat Thakur <83788445+vivdto@users.noreply.github.com>
2023-11-03 08:08:13 +0000

    created template for mymax().

---
+++ STL/templates.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+template <typename T>
+T mymax(T x, T y)
+{
+    return (x>y)?x:y;
+}
+
+int main ()
+{
+    cout<<mymax<int>(3,5)<<endl;
+    cout<<mymax<char>('c','g')<<endl;
+    
+    return 0;
+}
